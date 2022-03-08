4b650a900d028dae4532e12842cae517802d5d13
yasmine-glitch <yasmineberrada91@gmail.com>
2022-03-08 20:46:54 +0100

    V1 de la petite app fonctionnelle

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,6 +1,12 @@
-require 'poke-api-v2'
 class PokemonsController < ApplicationController
-  def show
-    @pokemon = Pokemon.find(params[:query])
+  require 'json'
+  require 'open-uri'
+  def index
+    if params[:query].present?
+      api_url = "https://pokeapi.co/api/v2/pokemon/#{params[:query].downcase}"
+      @pokemon = JSON.parse(URI.open(api_url).read)
+    else
+       redirect_to root_path
+    end
   end
 end
