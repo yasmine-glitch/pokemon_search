57c0c9703f4941bda5c34d18389f8532276e6fd4
yasmine-glitch <yasmineberrada91@gmail.com>
2022-03-14 18:38:49 +0100

    search sur l'index améliorée

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -2,7 +2,7 @@ class PokemonsController < ApplicationController
   require 'json'
   require 'open-uri'
   def index
-    if params[:query].present?
+    if params[:query].present? 
       api_url = "https://pokeapi.co/api/v2/pokemon/#{params[:query].downcase}"
       @pokemon = JSON.parse(URI.open(api_url).read)
     else
